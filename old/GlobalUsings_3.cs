74e03b9e357c5716894a19073b77bed539953244
Gabriel Lisboa Espindola Florencio <gabriel.florencio@caixa.gov.br>
2025-11-20 20:10:45 -0300

    v1.5.2 - Atualiza Readme e remove arquivos velhos

--- old/GlobalUsings_3.cs
+++
@@ -1,8 +0,0 @@
-global using InvestmentSimulation.API.Controllers;
-global using InvestmentSimulation.API.Extensions;
-global using InvestmentSimulation.API.Middlewares;
-global using InvestmentSimulation.Application.DTOs.Request;
-global using InvestmentSimulation.Application.DTOs.Response;
-global using InvestmentSimulation.Application.Interfaces;
-global using InvestmentSimulation.Domain.Exceptions;
-global using Microsoft.AspNetCore.Authorization;
